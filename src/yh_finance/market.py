8fc3a91f3fa0d04cbf4d592911f167d6e7f578dc
AG3NTZ3R0 <Z3R0@duck.com>
2022-03-10 17:17:42 -0500

    feat: added get_quotes to market.py

--- src/yh_finance/market.py
+++ src/yh_finance/market.py
@@ -1,2 +1,24 @@
-def main():
-    print("Hello World!")
+import requests
+
+
+def get_quotes(symbols: str, api_key: str, region: str):
+    """
+    Get live price quotes.
+
+    :param symbols: One or more comma seperated ticker symbols.
+    :param api_key: An API key from YH Finance API.
+    :param region: One of the following: US, BR, AU, CA, FR, DE, HK, IN, IT, ES, GB, SG.
+
+    :return: JSON API Response
+    """
+    url = "https://yh-finance.p.rapidapi.com/market/v2/get-quotes"
+    querystring = {"region": region, "symbols": symbols}
+    headers = {
+        'x-rapidapi-host': "yh-finance.p.rapidapi.com",
+        'x-rapidapi-key': api_key
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+
+    return response
+
