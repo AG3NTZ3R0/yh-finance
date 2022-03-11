dbf1fcf6d6a32f3c388d6a7e4e28588650632c27
AG3NTZ3R0 <Z3R0@duck.com>
2022-03-11 10:08:24 -0500

    feat: added get_earnings to market.py

--- src/yh_finance/market.py
+++ src/yh_finance/market.py
@@ -9,7 +9,7 @@ def get_quotes(region: str, symbols: str, api_key: str):
     :param symbols: One or more comma seperated ticker symbols.
     :param api_key: An API key from YH Finance API.
 
-    :return: API response in JSON
+    :return: API response in JSON.
     """
     url = "https://yh-finance.p.rapidapi.com/market/v2/get-quotes"
     querystring = {"region": region, "symbols": symbols}
@@ -30,11 +30,11 @@ def get_movers(region: str, lang: str, count: int, api_key: str, start: int = 0)
     :param region: One of the following: US, BR, AU, CA, FR, DE, HK, IN, IT, ES, GB, SG.
     :param lang: One of the following: en-US, pt-BR, en-AU, en-CA, fr-FR, de-DE, zh-Hant-HK, en-IN, it-IT, es-ES,
     en-GB, en-SG.
-    :param count: The number of items per response for paging purpose (max 25).
+    :param count: The number of items per response for paging purpose (Max 25).
     :param api_key: An API Key from YH Finance API.
     :param start: The starting offset for paging purpose.
 
-    :return: API response in JSON
+    :return: API response in JSON.
     """
     url = "https://yh-finance.p.rapidapi.com/market/v2/get-movers"
     querystring = {"region": region, "lang": lang, "count": count, "start": start}
@@ -55,10 +55,37 @@ def get_summary(region: str, api_key: str):
     :param region: One of the following: US, BR, AU, CA, FR, DE, HK, IN, IT, ES, GB, SG.
     :param api_key: An API Key from YH Finance API.
 
-    :return: API response in JSON
+    :return: API response in JSON.
     """
     url = "https://yh-finance.p.rapidapi.com/market/v2/get-summary"
     querystring = {"region": region}
+    headers = {
+        'x-rapidapi-host': "yh-finance.p.rapidapi.com",
+        'x-rapidapi-key': api_key
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+
+    return response
+
+
+def get_earnings(region: str, start_date: str, end_date: str, size: int, api_key: str):
+    """
+
+
+    :param region: region: One of the following: US, BR, AU, CA, FR, DE, HK, IN, IT, ES, GB, SG.
+    :param start_date: Unix timestamps in milliseconds. The value must be different from end_date and the value must be
+    the start of a day to get all the data from the day. The interval between start and end date is up to 14 days.
+    :param end_date: Unix timestamps in milliseconds. The value must be different from start_date and the value must be
+    the start of the later day to get all the data in the previous days. The interval between start and end date is up
+    to 14 days.
+    :param size: The number of items returned (Max 100).
+    :param api_key: An API Key from YH Finance API.
+
+    :return: API response in JSON.
+    """
+    url = "https://yh-finance.p.rapidapi.com/market/get-earnings"
+    querystring = {"region": region, "startDate": start_date, "endDate": end_date, "size": size}
     headers = {
         'x-rapidapi-host': "yh-finance.p.rapidapi.com",
         'x-rapidapi-key': api_key
