29b9e440970fe392799b48d5db6b32d9968a70ff
AG3NTZ3R0 <Z3R0@duck.com>
2022-03-12 07:05:19 -0500

    feat: added get_recommendations to stock.py

--- src/yh-finance/stock.py
+++ src/yh-finance/stock.py
@@ -5,7 +5,7 @@ def get_summary(symbol: str, region: str, api_key: str):
     """
     Get data in summary section.
 
-    :param symbol: The symbol to get data.
+    :param symbol: The symbol to get data for.
     :param region: One of the following: US, BR, AU, CA, FR, DE, HK, IN, IT, ES, GB, SG.
     :param api_key: An API key from YH Finance API.
 
@@ -16,6 +16,27 @@ def get_summary(symbol: str, region: str, api_key: str):
         "symbol": symbol,
         "region": region
     }
+    headers = {
+        'x-rapidapi-host': "yh-finance.p.rapidapi.com",
+        'x-rapidapi-key': api_key
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+
+    return response
+
+
+def get_recommendations(symbol: str, api_key: str):
+    """
+    Get symbols similar to the specified one.
+
+    :param symbol: The symbol to get data for.
+    :param api_key: An API key from YH Finance API.
+
+    :return: API response in JSON.
+    """
+    url = "https://yh-finance.p.rapidapi.com/stock/v2/get-recommendations"
+    querystring = {"symbol": symbol}
     headers = {
         'x-rapidapi-host': "yh-finance.p.rapidapi.com",
         'x-rapidapi-key': api_key
