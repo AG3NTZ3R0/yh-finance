465ebeea8edae149ba4c4d214c689037d1df80d8
AG3NTZ3R0 <Z3R0@duck.com>
2022-03-12 06:55:09 -0500

    feat: added get_summary to stock.py

--- src/yh-finance/stock.py
+++ src/yh-finance/stock.py
@@ -1,2 +1,26 @@
-def main():
-    print("Hello World!")
+import requests
+
+
+def get_summary(symbol: str, region: str, rapid_api: str):
+    """
+    Get data in summary section.
+
+    :param symbol: The symbol to get data.
+    :param region: One of the following: US, BR, AU, CA, FR, DE, HK, IN, IT, ES, GB, SG.
+    :param rapid_api: An API key from YH Finance API.
+
+    :return: API response in JSON.
+    """
+    url = "https://yh-finance.p.rapidapi.com/stock/v2/get-summary"
+    querystring = {
+        "symbol": symbol,
+        "region": region
+    }
+    headers = {
+        'x-rapidapi-host': "yh-finance.p.rapidapi.com",
+        'x-rapidapi-key': rapid_api
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+
+    return response
