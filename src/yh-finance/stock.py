e4bc0fe53677e11e3adc1b516ca19725aa6f8fe5
AG3NTZ3R0 <Z3R0@duck.com>
2022-03-12 08:59:55 -0500

    feat: added get_historical_data to stock.py

--- src/yh-finance/stock.py
+++ src/yh-finance/stock.py
@@ -133,3 +133,28 @@ def get_statistics(symbol: str, api_key: str):
     response = requests.request("GET", url, headers=headers, params=querystring).json()
 
     return response
+
+
+def get_historical_data(symbol: str, region: str, api_key: str):
+    """
+    Get data in historical data section.
+
+    :param symbol: The symbol to get data for.
+    :param region: One of the following: US, BR, AU, CA, FR, DE, HK, IN, IT, ES, GB, SG.
+    :param api_key: An API key from YH Finance API.
+
+    :return: API response in JSON.
+    """
+    url = "https://yh-finance.p.rapidapi.com/stock/v3/get-historical-data"
+    querystring = {
+        "symbol": symbol,
+        "region": region
+    }
+    headers = {
+        'x-rapidapi-host': "yh-finance.p.rapidapi.com",
+        'x-rapidapi-key': api_key
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+
+    return response
