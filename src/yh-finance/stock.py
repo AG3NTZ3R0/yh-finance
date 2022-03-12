81564462371e60943709113a5cd0b6ae1084c102
AG3NTZ3R0 <Z3R0@duck.com>
2022-03-12 10:17:51 -0500

    feat: added get_analysis to stock.py

--- src/yh-finance/stock.py
+++ src/yh-finance/stock.py
@@ -287,3 +287,28 @@ def get_balance_sheet(symbol: str, region: str, api_key: str):
     response = requests.request("GET", url, headers=headers, params=querystring).json()
 
     return response
+
+
+def get_analysis(symbol: str, region: str, api_key: str):
+    """
+    Get data in analysis section.
+
+    :param symbol: The symbol to get data for.
+    :param region: One of the following: US, BR, AU, CA, FR, DE, HK, IN, IT, ES, GB, SG.
+    :param api_key: An API key from YH Finance API.
+
+    :return: API response in JSON.
+    """
+    url = "https://yh-finance.p.rapidapi.com/stock/v2/get-analysis"
+    querystring = {
+        "symbol": symbol,
+        "region": region
+    }
+    headers = {
+        'x-rapidapi-host': "yh-finance.p.rapidapi.com",
+        'x-rapidapi-key': api_key
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+
+    return response
